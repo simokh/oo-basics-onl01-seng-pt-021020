d7edc40731fe3d9aa9ed2ecb1824cdc266c0dbfe
Simo Khouili <m.simokhouili@gmail.com>
2020-02-29 22:16:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_accessor :color,
                 :size,
                 :material,
-                :condition,
+                :condition
   attr_reader   :brand
 
   def initialize(brand)
